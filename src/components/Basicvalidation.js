83b1583180dcbd82303145959a077b923fca442f
Ishan Manandhar <ishan02016@gmail.com>
2020-03-12 14:18:38 +0545

    added basic validation in forms

--- src/components/Basicvalidation.js
+++ src/components/Basicvalidation.js
@@ -1,45 +1,57 @@
 import React, { Fragment } from 'react';
+import { useForm } from 'react-hook-form';
 
 export const Basicvalidation = () => {
+    const { handleSubmit, register, errors } = useForm();
+    const onSubmit = values => {
+        console.log(values, register)
+    }
     return (
         <Fragment>
             <div className="basicValidation">
                 <h4 className="font-weight-normal">Basic Validation</h4>
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="form-group">
                         <label htmlFor="exampleInputEmail1">First Name</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <input type="text" className="form-control" id="exampleInputEmail1" name="firstname" ref={register({ required: true })} placeholder="Your first name" />
+                        {errors.firstname && <span>This field is required</span>}
                     </div>
                     <div className="form-group">
                         <label htmlFor="exampleInputEmail1">Last Name</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <input type="text" className="form-control" id="exampleInputEmail1" name="lastnamename" ref={register({ required: true })} placeholder="Your last name" />
+                        {errors.firstname && <span>This field is required</span>}
                     </div>
                     <div className="form-group">
                         <label htmlFor="exampleInputEmail1">Email</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <input type="text" className="form-control" id="exampleInputEmail1" name="email"
+                            ref={register({ required: true })} placeholder="Your email" />
+                        {errors.firstname && <span>This field is required</span>}
                     </div>
                     <div>
                         <label htmlFor="validationTooltip04">Designation</label>
                         <select className="Title custom-select">
-                            <option value="Mr">Mr</option>
-                            <option value="Mrs">Mrs</option>
-                            <option value="Miss">Miss</option>
-                            <option value="Dr">Dr</option>
+                            <option value="">Choose one</option>
+                            <option value="Mr">Designer</option>
+                            <option value="Mrs">Frontend Developer</option>
+                            <option value="Miss">Backend Developer</option>
+                            <option value="Dr">Full Stack</option>
                         </select>
                     </div>
                     <br />
                     <div><b>Is Active</b></div>
                     <div>
-                        <div className="custom-control custom-radio">
-                            <input type="radio" id="customRadio1" name="customRadio" className="custom-control-input" />
+                        <div className="custom-control flex custom-radio">
+                            <input ref={register({ required: true })} type="radio" id="customRadio1" name="active" className="custom-control-input" />
+                            <div className="flex">{errors.active && <span>This field is required</span>}</div>
                             <label className="custom-control-label" htmlFor="customRadio1">Yes</label>
                         </div>
-                        <div className="custom-control custom-radio">
-                            <input type="radio" id="customRadio2" name="customRadio" className="custom-control-input" />
+                        <div className="custom-control flex custom-radio">
+                            <input ref={register({ required: true })} type="radio" id="customRadio2" name="active" className="custom-control-input" />
+                            <div className="flex">{errors.active && <span>This field is required</span>}</div>
                             <label className="custom-control-label" htmlFor="customRadio2">No</label>
                         </div>
                     </div>
-                    <button type="submit" className="btn btn-primary btn-lg btn-block">Submit</button>
+                    <button className="btn btn-primary btn-lg btn-block">Submit</button>
                 </form>
             </div>
         </Fragment>
