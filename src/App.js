5524931cf97ab02bfeecb193d8132fbb804b4b7d
Ishan Manandhar <ishan02016@gmail.com>
2020-03-12 12:21:56 +0545

    added needed packages and setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h2>Form Validation with React Hook Form</h2>
+      {/* Please refer branches for code */}
     </div>
   );
 }
